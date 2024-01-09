bd6601810ef651ff2d9686b90b40c9ca4fcd70cc
AnnOvs19 <annaovsyanikowa@yandex.ru>
2024-01-09 22:34:43 +0300

    Creating header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 
+import headerLogo from "../../assets/icons/header-logo.svg";
+import headerSearch from "../../assets/icons/header-search.svg";
+import headerBasket from "../../assets/icons/header-basket.svg";
+
 import "./header.scss";
 
 const Header = (props) => {
     return (
-        <div className='header'>
-            grvl
-        </div>
+        <header className='header'>
+            <img src={headerLogo} alt="#" />
+            <nav className='header__nav'>
+                <ul>
+                    <li>Catalog</li>
+                    <li>Blogs</li>
+                    <li>About as</li>
+                </ul>
+            </nav>
+            <div className='header__user'>
+                <img src={headerSearch} alt="#" />
+                <img src={headerBasket} alt="#" />
+            </div>
+        </header>
     );
 };
 
