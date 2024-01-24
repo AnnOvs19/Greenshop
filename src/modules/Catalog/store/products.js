260a1afc0e599a78ec04a060c28539476b4c6e7a
AnnOvs19 <annaovsyanikowa@yandex.ru>
2024-01-24 23:19:15 +0300

    fixed images

---
+++ src/modules/Catalog/store/products.js
@@ -0,0 +1,75 @@
+import marketingLily from '../../../assets/product-images/catalog-lily4.png';
+import catalogBegonia2 from '../../../assets/product-images/catalog-begonia2.png';
+import violet3 from '../../../assets/product-images/catalog-violet3.png';
+import bromeliad5 from '../../../assets/product-images/catalog-bromeliad5.png';
+import plant6 from '../../../assets/product-images/catalog-plant6.png';
+import fern7 from '../../../assets/product-images/catalog-fern7.png';
+import palm8 from '../../../assets/product-images/catalog-palm8.png';
+import evegreen9 from '../../../assets/product-images/catalog-evegreen9.png';
+
+
+export const productsArray = [
+    {
+        id: 1,
+        title: 'Barberton Daisy',
+        price: 119.00,
+        image: marketingLily,
+        brand: 1
+    },
+    {
+        id: 2,
+        title: "Angel Wing Begonia",
+        price: 169.00,
+        image: catalogBegonia2,
+        brand: 2
+    },
+    {
+        id: 3,
+        title: "African Violet",
+        price: 199.00,
+        image: violet3,
+        brand: 3
+    },
+    {
+        id: 4,
+        title: "Beach Spider Lily",
+        price: 129.00,
+        image: marketingLily,
+        brand: 4
+    },
+    {
+        id: 5,
+        title: "Blushing Bromeliad",
+        price: 139.00,
+        image: bromeliad5,
+        brand: 5
+    },
+    {
+        id: 6,
+        title: 'Aluminum Plant',
+        price: 179.00,
+        image: plant6,
+        brand: 6
+    },
+    {
+        id: 7,
+        title: "Bird's Nest Fern",
+        price: 99.00,
+        image: fern7,
+        brand: 7
+    },
+    {
+        id: 8,
+        title: "Broadleaf Lady Palm",
+        price: 59.00,
+        image: palm8,
+        brand: 8
+    },
+    {
+        id: 9,
+        title: 'Chinese Evergreen',
+        price: 39.00,
+        image: evegreen9,
+        brand: 9
+    }
+]
